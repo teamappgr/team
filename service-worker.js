76f24b08a7c333dbd701ebe82a971b1578fbf183
lastchancecy <giorgos.giorgos2003@gmail.com>
2024-09-29 21:40:11 +0300

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// public/service-worker.js
+
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : {};
+    const options = {
+        body: data.message || 'You have a new notification!',
+        icon: 'origin.png', // Path to an icon
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title || 'Notification', options)
+    );
+});
+
+// Optionally, you can also handle notification click events
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    // Handle the click event
+    event.waitUntil(
+        clients.openWindow(process.env.PUBLIC_URL   ) // Change to your desired URL
+    );
+});
