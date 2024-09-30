5dc2a171ab8d29c1d6993960fc533d267a22a8c1
lastchancecy <giorgos.giorgos2003@gmail.com>
2024-09-30 03:38:45 +0300

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -551,6 +551,23 @@ app.delete('/subscriptions/:userId', async (req, res) => {
       res.status(500).json({ message: 'Error deleting subscription' });
   }
 });
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const userData = result.rows[0]; // Get the user data from the query result
+    res.json(userData);
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 // Start the server
 server.listen(PORT, () => {
