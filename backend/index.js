abfbe0569a5b285c8ee58428ca98c33bf3957bc0
lastchancecy <giorgos.giorgos2003@gmail.com>
2024-10-26 00:31:48 +0300

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1485,6 +1485,6 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = (req, res) => {
+  app(req, res); // Export as serverless function
+};
