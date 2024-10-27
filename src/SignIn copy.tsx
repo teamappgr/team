cd4fbd77ab8b72938c43b38fb2ba4de2c5efe5eb
lastchancecy <giorgos.giorgos2003@gmail.com>
2024-10-28 01:09:36 +0200

    initial commit

--- src/SignIn copy.tsx
+++ src/SignIn copy.tsx
@@ -23,7 +23,7 @@ function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="/about">
+      <Link color="inherit" href="/team/#/about">
         TeamGr
       </Link>{' '}
       {new Date().getFullYear()}
